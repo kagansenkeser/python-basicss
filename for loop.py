f4b6079f260b664981a59f9398e763956872fb81
kagansenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-02-15 17:17:47 +0300

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -1,1 +1,9 @@
+#how to use if else
+number=input("for yes 1  for no 2\n")
+#for example in this example we are taking an str value so have to tranfrom it to int value
+intnumber=int(number)
 
+if intnumber==1:
+    print("yes")
+else:
+    print("no")
