2f3e259bcf656b6cb50fa85132d2cae703534bce
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 20:34:25 +0300

    Update and rename first code print.py to 1.ilkkod print.py

---
+++ 1.ilkkod print.py
@@ -0,0 +1,4 @@
+# merhaba burda sizlere python hakkında bildiklerimden kısa bir derleme yapmaya çalışacağım
+print("merhaba dünya")
+#yorum satırları icin # i kullanıyoruz c nin aksine printf yerine print dememiz yeterli ; de kullanmıyoruz 
+#ve herhangi bir gövde kullanmıyoruz
