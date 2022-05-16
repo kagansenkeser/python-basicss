c5362f8cbd70b9ac3085536feb9d574cbc1bb8e8
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 20:42:11 +0300

    Create 3.matematik islemleri.py

---
+++ 3.matematik islemleri.py
@@ -0,0 +1,14 @@
+#matematiksel islemler
+a=5
+b=3
+
+#toplama icin +
+#cikarma icin -
+#carpma icin *
+#bolme icin /
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#c deki ile ayni mantikda isliyor tek farki %d ile ugrasmamiza
+#gerek kalmiyor
