5438b036ad1802a5b84d0e01aeef2aa67df57fd0
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 20:39:01 +0300

    Create 2.veri tipleri.py

---
+++ 2.veri tipleri.py
@@ -0,0 +1,12 @@
+#burda veri tiplerini görecegiz
+# c nin aksine python da tipi belirtmemiz gerekmyior
+#ornegin
+a=10
+b=2.6
+c="merhaba"
+
+print(type(a))
+print(type(b))
+print(type(c))
+#tiplerini gormek icin type()fonksiyonunu kullaniyoruz
+#gordgunuz gibi python tipleri kendisi anlayip veriyor
