60e56a441ee6b72dd12299424c9e38f1b3dd124a
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 20:44:43 +0300

    Create kullanicidan deger alma.py

---
+++ kullanicidan deger alma.py
@@ -0,0 +1,6 @@
+#kullanicidan deger alma
+x=input("lutfen deger veriniz ")
+#input() komutu ile kullanicdan deger alabiliyoruz
+#icindeki bosluga da mesajimizi yaziyoruz
+print("verdiginiz deger",x)
+# c dekinin aksine önce yazi yazip , ile sayi yazdırabiliyoruz print komutu ile
