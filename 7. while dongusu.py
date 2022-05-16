dbd229843d4d66b0eab4a64848bf93a2f55cc441
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 21:05:15 +0300

    Create 7. while dongusu.py

---
+++ 7. while dongusu.py
@@ -0,0 +1,8 @@
+#while dongusu
+x=1
+while x<5:
+    print(x)
+    x=x+1
+#while sart:
+#(tab) sart saglaniyorsa yapilacaklar
+#while dongusunun baska versiyonlari da mevcut bu en sıradan ve kolay olanı
