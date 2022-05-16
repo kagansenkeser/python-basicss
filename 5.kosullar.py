cd1b2bf541a4b75c46880572bd494c1874ae9fc1
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 20:51:39 +0300

    Create 5.kosullar.py

---
+++ 5.kosullar.py
@@ -0,0 +1,15 @@
+#evet bir dilin en önemli konusu if else
+#burda da python c ye kiyasla buyuk bir rahatlik sunuyor
+
+yas=15
+
+if yas<18:
+    print("yetiskin degilsiniz")
+else:
+    print("yetiskinsiniz")
+#pyton daki dikkat edilmesi gereken durum if den sonraki 
+#islemlerin gerceklesecegi yerde (tab) boslugu birakilmalidir
+#if sart:
+#   sart dogru ise yapilacaklar
+#else:
+#   sart doru degil ise yapilacaklar
